bae8f2120c97cf9cd8892e6769bc73a16c2e5d35
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-22 20:13:24 +0530

    Added:Method First name added with test cases

---
+++ src/main/java/UserValidator.java
@@ -0,0 +1,10 @@
+import java.util.regex.Pattern;
+
+public class  UserValidator {
+        private final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+
+        public boolean validateFirstName(String firstName) {
+            Pattern pattern = Pattern.compile(NAME_PATTERN);
+            return pattern.matcher(firstName).matches();
+             }
+}
